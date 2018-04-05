9309e330a8a886a8dfb87994505d048db88c5d09
Theodore hughes <theodorehughes@Theodores-MacBook-Pro.local>
2018-04-05 16:50:40 -0500

    connected to db successfully

--- bamazon.js
+++ bamazon.js
@@ -2,11 +2,19 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("console.table");
-// connects to mySQL db
+//sets up connection info for mySQL
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "bamazon"
+});
+
+//connects to the database
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+    }
+    console.log("connected to bamazon database!");
 });
